370bed35ee82ce09198f51a0a8793cde37dd9b53
vktistopoulos <t8130076@dias.aueb.gr>
2015-01-10 01:08:06 +0200

    Delete Fyllo.java

--- Fyllo.java
+++
@@ -1,8 +0,0 @@
-public final class Fyllo  {
-public final int symbol;
-public Fyllo(int symbol) {
-if (symbol < 0)
-throw new IllegalArgumentException("IllegalArgumentException");
-this.symbol = symbol;
-}
-}
