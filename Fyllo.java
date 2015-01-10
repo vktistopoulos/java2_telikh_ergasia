1963402a6caefb90fc1099b07ac639752babdc4d
ZoiP <zoip1995@gmail.com>
2015-01-10 15:39:19 +0200

    Update Fyllo.java

--- Fyllo.java
+++ Fyllo.java
@@ -1,6 +1,12 @@
+public final class Fyllo extends Komvos {
+  
   public final int symbol;
-   
--  public Fyllo  int symbol {
-+  public Fyllo ( int symbol) {
-     if (symbol < 0)
-       throw new IllegalArgumentException("IllegalArgumentException");
+  
+  public Fyllo ( int symbol) {
+    if (symbol < 0)
+      throw new IllegalArgumentException("IllegalArgumentException");
+      
+      this.symbol = symbol;
+  }
+}
+
